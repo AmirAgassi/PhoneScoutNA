9a7fd04cb8dcdbbb8eb648b0201d9cd8e4e76a7b
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-09-22 09:44:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -71,19 +71,6 @@ app.get('/', (req, res) => {
   res.status(200).send("Hello!");
 });
 
-app.get('/email', async (req, res) => {
-  try {
-    const apiUrl = 'http://api.bulkoutlook.com/getEmail?num=1&no=IylenqlvLtSxxNHiLVlmWIuRvrJtFiYEyUjyFgxnHieaiJat&emailType=hotmail&format=txt';
-    const response = await axios.get(apiUrl);
-    const data = response.data;
-
-    res.header("Content-Type", 'text/plain');
-    res.send(data);
-  } catch (error) {
-    res.status(500).send('Error fetching email');
-  }
-});
-
 app.get('/lookup/:phonenumber', (req, res) => {
   let phonenumber = req.params['phonenumber'];
   phonenumber = phonenumber.replace('%20', '+');
